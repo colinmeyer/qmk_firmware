13269282af65d5332cbe247712e6b11ab5c28a20
Colin Meyer <cmeyer@helvella.org>
2021-02-24 00:57:19 -0800

    simplyfying dance logic -- does not seem like interrupt is right for DOUBLE_SINGLE_TAP

--- keyboards/preonic/keymaps/colinmeyer/keymap.c
+++ keyboards/preonic/keymaps/colinmeyer/keymap.c
@@ -249,21 +249,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 /* tap dance for MT functions w/ mods */
 uint8_t cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (!state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
+        if (state->pressed) return SINGLE_HOLD;
+        else return SINGLE_TAP;
     }
+
     if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
+        if (state->pressed) return DOUBLE_HOLD;
         else return DOUBLE_TAP;
     }
 
-    // Assumes no one is trying to type the same letter three times (at least not quickly).
-    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
-    // an exception here to return a 'TRIPLE_SINGLE_TAP', and define that enum just like 'DOUBLE_SINGLE_TAP'
     if (state->count == 3) {
-        if (state->interrupted || !state->pressed) return TRIPLE_TAP;
-        else return TRIPLE_HOLD;
+        if (state->pressed) return TRIPLE_HOLD;
+        else return TRIPLE_TAP;
     } else return 8; // Magic number. At some point this method will expand to work for more presses
 }
 
