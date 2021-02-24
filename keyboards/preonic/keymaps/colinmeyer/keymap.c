3052349308152570cb0ceb4bbcb8b169f55779f1
Colin Meyer <cmeyer@helvella.org>
2021-02-24 00:17:01 -0800

    working on tap dance as MT

--- keyboards/preonic/keymaps/colinmeyer/keymap.c
+++ keyboards/preonic/keymaps/colinmeyer/keymap.c
@@ -250,13 +250,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 uint8_t cur_dance(qk_tap_dance_state_t *state) {
     if (state->count == 1) {
         if (!state->pressed) return SINGLE_TAP;
-        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
         else return SINGLE_HOLD;
     }
     if (state->count == 2) {
-        // DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
-        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
-        // keystrokes of the key, and not the 'double tap' action/macro.
         if (state->interrupted) return DOUBLE_SINGLE_TAP;
         else if (state->pressed) return DOUBLE_HOLD;
         else return DOUBLE_TAP;
@@ -282,13 +278,6 @@ void os_finished(qk_tap_dance_state_t *state, void *user_data) {
     switch (xtap_state.state) {
         case SINGLE_TAP: register_code16(G(KC_TAB)); break;
         case SINGLE_HOLD: register_code16(KC_LGUI); break;
-
-        // case DOUBLE_TAP: register_code(KC_ESC); break;
-        // case DOUBLE_HOLD: register_code(KC_LALT); break;
-        // Last case is for fast typing. Assuming your key is `f`:
-        // For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
-        // In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
-        // case DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code(KC_X);
     }
 }
 
@@ -296,10 +285,6 @@ void os_reset(qk_tap_dance_state_t *state, void *user_data) {
     switch (xtap_state.state) {
         case SINGLE_TAP: unregister_code16(G(KC_TAB)); break;
         case SINGLE_HOLD: unregister_code16(KC_LGUI); break;
-
-        // case DOUBLE_TAP: unregister_code(KC_ESC); break;
-        // case DOUBLE_HOLD: unregister_code(KC_LALT);
-        // case DOUBLE_SINGLE_TAP: unregister_code(KC_X);
     }
     xtap_state.state = 0;
 }
