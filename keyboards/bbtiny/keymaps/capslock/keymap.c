916bb572fb6a112a209eec122f5b5ed4fb216340
Colin Meyer <cmeyer@helvella.org>
2021-03-03 01:55:53 -0800

    capslock is the button, and the green led the indicator

---
+++ keyboards/bbtiny/keymaps/capslock/keymap.c
@@ -0,0 +1,34 @@
+/* Copyright 2021 nilc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+    KC_CAPS
+  )
+};
+
+void keyboard_pre_init_user(void) {
+  // Set our LED pins as output
+  setPinOutput(B1);
+}
+
+bool led_update_user(led_t led_state) {
+    writePin(B1, led_state.caps_lock);
+    return false;
+}
+
