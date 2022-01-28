f95a383cf28f8e50bbafba869d3348ee7de0b754
Colin Meyer <cmeyer@helvella.org>
2022-01-28 09:50:20 -0800

    MOUSEKEY_ENABLE

--- keyboards/splitkb/kyria/keymaps/colinmeyer/rules.mk
+++ keyboards/splitkb/kyria/keymaps/colinmeyer/rules.mk
@@ -1,5 +1,6 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+MOUSEKEY_ENABLE = yes
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 SRC += colinmeyer.c        # good stuff
